4269960c470477354e3340b59363ccfe47fa0478
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2025-11-16 17:19:44 +0000

    initial

---
+++ src/main/java/com/parkour/ParkourPlugin.java
@@ -0,0 +1,110 @@
+package com.parkour;
+
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.Perspective;
+import net.runelite.api.Player;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.ClientTick;
+import net.runelite.api.events.MenuOptionClicked;
+import net.runelite.api.events.StatChanged;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
+@PluginDescriptor(
+		name = "Parkour!", description = "Randomly generated overhead text whilst training agility"
+)
+public class ParkourPlugin extends Plugin {
+	@Inject
+	private Client client;
+	private final Random random = new Random();
+	private int OVERHEAD_CYCLE = 100;
+	private int MAX_OVERHEAD_TRIGGER = 5;
+	private int obstacleCount;
+	private int obstacleCountTrigger;
+	private int randomElement;
+	private boolean firstAgilityEvent = true;
+	private LocalPoint markOfGraceLoc;
+	private final List<String> overheadTextList = List.of(
+			"PAAAAAAAAAAAAAARKOOOOOOOUR!!!",
+			"Glideeeeeeeeeeee!",
+			"WOAH I ALMOST FELL!",
+			"Smoooooooth landing!"
+	);
+
+
+	public ParkourPlugin() {
+	}
+
+	protected void startUp() throws Exception
+	{
+		randomiseObstacleLimit();
+	}
+	public void randomiseObstacleLimit()
+	{
+		obstacleCountTrigger = random.nextInt(MAX_OVERHEAD_TRIGGER) + 5;
+	}
+
+	public void randomiseOverheadText()
+	{
+		randomElement = random.nextInt(overheadTextList.size());
+	}
+
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked e)
+	{
+		if (e.getMenuOption().equals("Take") && e.getMenuTarget().endsWith("Mark of grace"))
+		{
+			markOfGraceLoc = LocalPoint.fromScene(e.getParam0(),e.getParam1());
+		}
+		else
+		{
+			markOfGraceLoc = null;
+		}
+	}
+	@Subscribe
+	public void onClientTick(ClientTick tick)
+	{
+		if (markOfGraceLoc != null)
+		{
+			Player player = client.getLocalPlayer();
+			if (markOfGraceLoc.distanceTo(player.getLocalLocation()) <= Perspective.LOCAL_TILE_SIZE) {
+				player.setOverheadText("YOINK!");
+				player.setOverheadCycle(OVERHEAD_CYCLE);
+				markOfGraceLoc = null;
+			}
+		}
+	}
+	@Subscribe
+	public void onStatChanged(StatChanged stat)
+	{
+		if (!stat.getSkill().getName().equals("Agility"))
+		{
+			return;
+		}
+		// makes sure plugin doesnt fire on startup
+		if (firstAgilityEvent)
+		{
+			firstAgilityEvent = false;
+			return;
+		}
+		obstacleCount = obstacleCount + 1;
+
+		// checks if enough xp drops have happened to trigger overhead text
+		if (obstacleCount == obstacleCountTrigger)
+		{
+			Player player = client.getLocalPlayer();
+			randomiseOverheadText();
+			player.setOverheadText(overheadTextList.get(randomElement));
+			player.setOverheadCycle(OVERHEAD_CYCLE);
+			obstacleCount = 0;
+			randomiseObstacleLimit();
+		}
+	}
+}
--- src/test/java/com/parkour/ParkourPluginTest.java
+++ src/test/java/com/parkour/ParkourPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.parkour;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ParkourPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ParkourPlugin.class);
 		RuneLite.main(args);
 	}
 }
